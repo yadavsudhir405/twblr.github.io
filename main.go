d521e52f495af6bd8f04936e75560da8ece8d52a
Rajeev N B <rajeevrvis@gmail.com>
2016-07-18 00:05:39 +0530

    Add code for defer, panic and recover

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	goDefer()
+	goPanic()
+}
+
+func goDefer() {
+	fmt.Println("Start Function")
+	err := fmt.Errorf("I have failed you!")
+
+	defer func() {
+		fmt.Println("Start Defer")
+		if err != nil {
+			fmt.Println("Defer Error:", err)
+		}
+	}()
+
+	fmt.Println("End Test")
+}
+
+func goPanic() {
+	defer func() {
+		fmt.Println("Start Panic Defer")
+
+		if r := recover(); r != nil {
+			fmt.Println("Defer Panic:", r)
+		}
+	}()
+
+	fmt.Println("Start Test")
+	panic("I am the trouble")
+}
