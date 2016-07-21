1e87fa0f3ce8be9c99e54da43b0f0e8f47d1d7f0
Rajeev N B <rajeevrvis@gmail.com>
2016-07-21 22:46:58 +0530

    add collatz conjecture prob

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,12 +1,5 @@
 package control_structures
 
-type mapOperation func(int32) int32
-type filterOperation func(int32) bool
-
-func mapInts(op mapOperation, vals []int32) []int32 {
-	return []int32{}
-}
-
-func filterInts(op filterOperation, vals []int32) []int32 {
-	return []int32{3}
+func collatzChainLength(n int) int {
+	return 0
 }
--- control_structures/for_structure_test.go
+++ control_structures/for_structure_test.go
@@ -2,42 +2,10 @@ package control_structures
 
 import "testing"
 
-func TestMapInts(t *testing.T) {
+func TestCollatzChainLength(t *testing.T) {
+	n := collatzChainLength(27)
 
-	square := func(arg1 int32) int32 {
-		return arg1 * arg1
-	}
-
-	res := mapInts(square, []int32{1, 2, 3})
-	expectedResult := []int32{1, 4, 9}
-
-	if len(res) < 1 {
-		t.Errorf("Expected the result to be %v, but got %v", expectedResult, res)
-	}
-
-	for i := range res {
-		if res[i] != expectedResult[i] {
-			t.Errorf("Expected the result to be %v, but got %v", expectedResult, res)
-		}
-	}
-}
-
-func TestFilterInts(t *testing.T) {
-
-	greaterThanTwo := func(arg1 int32) bool {
-		return arg1 > 2
-	}
-
-	res := filterInts(greaterThanTwo, []int32{1, 2, 3})
-	expectedResult := []int32{3}
-
-	if len(res) < 1 {
-		t.Errorf("Expected the result to be %v, but got %v", expectedResult, res)
-	}
-
-	for i := range res {
-		if res[i] != expectedResult[i] {
-			t.Errorf("Expected the result to be %v, but got %v", expectedResult, res)
-		}
+	if n != 111 {
+		t.Errorf("Expected %d, got %d", 111, n)
 	}
 }
