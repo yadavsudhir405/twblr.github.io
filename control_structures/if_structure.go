0a4d7044f3857e4fd46d2a612380346577e10264
sudhir.yadav <sudhir.yadav@mu-sigma.com>
2016-07-22 15:27:52 +0530

    Initial Commit

--- control_structures/if_structure.go
+++ control_structures/if_structure.go
@@ -1,5 +1,19 @@
 package control_structures
 
 func fizzBuzz(i int32) string {
-	return ""
+	result:="Invalied"
+	if i%2==0{
+		result="2"
+	}else if i%3==0{
+		result="Fizz"
+	}else if i%5==0{
+		result="Buzz"
+	}else if i%15==0{
+		result="FizzBuzz"
+	}else if i%999==0{
+		result="Fizz"
+	}else{
+		result="Invalid"
+	}
+	return result
 }
