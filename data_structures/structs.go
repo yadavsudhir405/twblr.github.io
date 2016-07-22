6229ba2e1c1c9c636a27009d694a0f98fc21894a
sudhir.yadav <sudhir.yadav@mu-sigma.com>
2016-07-23 00:18:28 +0530

    Added Struct test

--- data_structures/structs.go
+++ data_structures/structs.go
@@ -2,12 +2,33 @@ package data_structures
 
 // Linked List Implementation
 
-type Node struct{}
-
-func (n &Node)Add(elem int) {
-	return
+type Node struct{
+	data int
+	next *Node
+}
+var head *Node
+func Add(elem int) {
+	if head==nil {
+		head=new(Node)
+		head.data=elem
+	}else{
+		temp:=head
+		for temp.next!=nil{
+			temp=temp.next
+		}
+		newNode:=new(Node)
+		newNode.data=elem
+		temp.next=newNode
+	}
 }
 
 func Search(elem int) *Node {
+	temp:=head
+	for temp!=nil{
+		if temp.data==elem {
+			return temp
+		}
+		temp=temp.next
+	}
 	return nil
 }
