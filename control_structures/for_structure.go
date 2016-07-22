55063c784553a264d91230ebdaed4349d441e411
sudhir.yadav <sudhir.yadav@mu-sigma.com>
2016-07-22 16:05:56 +0530

    Added fortest

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,5 +1,14 @@
 package control_structures
 
 func collatzChainLength(n int) int {
-	return 0
+	iteration:=0
+	for n!=1{
+		if n%2==0{
+			n=n/2
+		}else{
+			n=3*n+1
+		}
+		iteration++
+	}
+	return iteration
 }
