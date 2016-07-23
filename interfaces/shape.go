8143f9c61db2cb7645c66d9ed64e52df991cdbd7
sudhir.yadav <sudhir.yadav@mu-sigma.com>
2016-07-23 10:27:10 +0530

    Added Shape

--- interfaces/shape.go
+++ interfaces/shape.go
@@ -3,3 +3,23 @@ package interfaces
 type Shape interface {
 	Area() int
 }
+type Square struct{
+	side int
+}
+func (s *Square) Area()int{
+	return s.side*s.side
+}
+type Rectangle struct{
+	length int
+	breadth int
+}
+func (r *Rectangle) Area() int{
+	return r.length*r.breadth
+}
+type Hybrid struct{
+	square Square
+	rectangle Rectangle
+}
+func (h *Hybrid) Area() int{
+	return h.square.Area() +h.rectangle.Area()
+}
